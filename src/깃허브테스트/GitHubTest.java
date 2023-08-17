7f42b11db0ce3dfc0895ba9ebc5d7901daff923f
hjck4433 <hjck4433@gmail.com>
2023-08-17 22:18:21 +0900

    집에서 깃으로 푸쉬

---
+++ src/깃허브테스트/GitHubTest.java
@@ -0,0 +1,8 @@
+package 깃허브테스트;
+
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("깃 허브에 잘 push가 되는지 확인 해 봅니다!");
+        System.out.println("푸쉬가 잘 되면 내일 학원에서 pull 를 사용해봅니다!");
+    }
+}
